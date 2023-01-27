ee1e9b1d07d73fbeb37c6ebc44a9c3e176c0862e
mikeycrystal <michaelcrystal87@gmail.com>
2023-01-27 14:02:16 -0600

    Tailwind Initial Style

---
+++ components/navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const navbar = () => {
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between"></div>
+    </div>
+  );
+};
+
+export default navbar;
