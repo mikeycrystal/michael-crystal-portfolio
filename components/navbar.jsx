234547aff3fdeccba658a177e3b5f2394fb69245
mikeycrystal <michaelcrystal87@gmail.com>
2023-01-29 14:05:34 -0600

    Styling + Add logo

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
-const navbar = () => {
+const Navbar = () => {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
-      <div className="flex justify-between"></div>
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image src="../public/assets/logo.png" alt="/" width="205" height="5" />
+      </div>
+      <div>
+        <ul>
+          <Link href="/">
+            <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+          </Link>
+        </ul>
+      </div>
     </div>
   );
 };
 
-export default navbar;
+export default Navbar;
